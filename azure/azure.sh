aa14820bf9458b26ca20c501e27ca4e6a6c59358
obanach <obanach@outlook.com>
2023-12-30 00:49:27 +0100

    setup nelmio cors

--- azure/azure.sh
+++ azure/azure.sh
@@ -1,3 +1,3 @@
 cp /home/site/wwwroot/azure/nginx /etc/nginx/sites-available/default
 service nginx restart
-#php bin/console lexik:jwt:generate-keypair --skip-if-exists
+php bin/console lexik:jwt:generate-keypair --skip-if-exists

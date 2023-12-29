54bce169af80bc667b1bcfa6d81e7021586fd23b
obanach <obanach@outlook.com>
2023-12-29 19:37:19 +0100

    add nelmio cors

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
